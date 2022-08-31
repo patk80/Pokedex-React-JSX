f87d4eec63f18be4e390db66da13dc66eb5fc767
Pat Kelly <PatK80@gmail.com>
2022-08-30 21:46:04 -0400

    Adding in components folder and Bootstrap

---
+++ src/components/AlbumCard.jsx
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+
+
+function AlbumCard() {
+    return (
+      <div className="Album-container">
+        <h2>Ride the Lightning</h2>
+        <img className="Album-artwork" src="/img/ride-the-lightning-album-cover.jpeg" alt="ride the lightning album cover" />
+        <p>1984</p>
+      </div>
+    );
+  }
+  
+export default AlbumCard;

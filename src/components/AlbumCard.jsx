6df7dffb82771aaf210ffc44eb8c4a976a32f1d2
Pat Kelly <PatK80@gmail.com>
2022-08-30 21:55:31 -0400

    Adding json file and minor updates

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -4,9 +4,13 @@
 function AlbumCard() {
     return (
       <div className="Album-container">
-        <h2>Ride the Lightning</h2>
-        <img className="Album-artwork" src="/img/ride-the-lightning-album-cover.jpeg" alt="ride the lightning album cover" />
-        <p>1984</p>
+        <div className="row">
+          <div className="col-md-4">
+            <h2>Ride the Lightning</h2>
+            <img className="Album-artwork" src="/img/ride-the-lightning-album-cover.jpeg" alt="ride the lightning album cover" />
+            <p>1984</p>
+          </div>
+        </div>
       </div>
     );
   }
